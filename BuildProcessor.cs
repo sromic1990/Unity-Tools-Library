2ed990f1194bb4c97d58cb36bca29e4cef32b908
Sourav Chatterjee <sromic@gmail.com>
2021-05-10 19:04:59 +0530

    Minor changes in BuildProcessor

--- BuildProcessor.cs
+++ BuildProcessor.cs
@@ -17,34 +17,35 @@ namespace UnityToolsLibrary
             string plistPath = pathToBuiltProject + "/Info.plist";
             PlistDocument plist = new PlistDocument();
             plist.ReadFromString(File.ReadAllText(plistPath));
- 
+
             // Get root
             PlistElementDict rootDict = plist.root;
- 
+
             // Set encryption usage boolean
             string encryptKey = "ITSAppUsesNonExemptEncryption";
             rootDict.SetBoolean(encryptKey, false);
- 
+
             // remove exit on suspend if it exists.
             string exitsOnSuspendKey = "UIApplicationExitsOnSuspend";
-            if(rootDict.values.ContainsKey(exitsOnSuspendKey))
+            if (rootDict.values.ContainsKey(exitsOnSuspendKey))
             {
                 rootDict.values.Remove(exitsOnSuspendKey);
             }
 
             //remove Metal from Required device capabilities
             string requiredDeviceCapabilities = "UIRequiredDeviceCapabilities";
-            if(rootDict.values.ContainsKey(requiredDeviceCapabilities))
+            if (rootDict.values.ContainsKey(requiredDeviceCapabilities))
             {
                 rootDict.values.Remove(requiredDeviceCapabilities);
-            
+
                 var key = "UIRequiredDeviceCapabilities";
                 PlistElementArray array = rootDict.CreateArray(key);
                 array.AddString("armv7");
             }
- 
+
             // Write to file
             File.WriteAllText(plistPath, plist.WriteToString());
 #endif
         }
     }
+}
